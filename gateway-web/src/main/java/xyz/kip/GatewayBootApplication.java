862e61e6f4e8b71381e6330ab903ea4f4a70d264
xiaoshichuan <xiaoshichuan94@gmail.com>
2026-02-28 02:05:30 +0800

    fix initial

---
+++ gateway-web/src/main/java/xyz/kip/GatewayBootApplication.java
@@ -0,0 +1,13 @@
+package xyz.kip;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author xiaoshichuan
+ * @version 2026-02-27 23:33
+ */
+public class GatewayBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayBootApplication.class, args);
+    }
+}
